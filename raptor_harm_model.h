ece92db7f4f1a024eabbe00bbc8cbf5c56258b1e
Thomas Bronzwaer <thomasbronzwaer@tb-mbp16-2.home>
2021-06-18 13:52:19 +0200

    Deleted raptor_harm_model.c as only the 3d version is used; still must get rid of the .h file though

--- raptor_harm_model.h
+++ raptor_harm_model.h
@@ -14,8 +14,8 @@
 
 #include <stdio.h>
 
-#define NDIM 4
-#define NPRIM 8
+#define NDIM	4
+#define NPRIM	8
 
 double ****econ;
 double ****ecov;
@@ -34,16 +34,16 @@ int n_within_horizon;
 double Ladv, dMact;
 
 /* mnemonics for primitive vars; conserved vars */
-#define KRHO 0
-#define UU 1
-#define U1 2
-#define U2 3
-#define U3 4
-#define B1 5
-#define B2 6
-#define B3 7
+#define KRHO     0
+#define UU      1
+#define U1      2
+#define U2      3
+#define U3      4
+#define B1      5
+#define B2      6
+#define B3      7
 
-#define TP_OVER_TE (0.)
+#define TP_OVER_TE	(0.)
 
 /* some coordinate parameters */
 double R0, Rin, Rh, Rout, Rms;
@@ -54,7 +54,7 @@ double dlE, lE0;
 double gam;
 double dMsim;
 
-// double M_unit;
+//double M_unit;
 double L_unit;
 double T_unit;
 double RHO_unit;
