2874499c7f378dab6a545121b8e26581e188b842
Jordy Davelaar <jdavelaar@pcn-10-52.cm.cluster>
2022-03-16 13:56:23 -0700

    some safety checks...

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@ echo "seting up code for" $1
 
 echo " "
 
+if test -f "raptor_model.c"; then
+    echo "raptor_model.c exists. and would be overwritten."
+    echo "Only use this script in empty directories."
+    echo "Aborting..."
+    exit
+fi
 
 if [ "$RAPTOR" == "" ] ;
 then
@@ -19,18 +25,19 @@ fi
 
 cp $RAPTOR/run/* .
 
+echo "Copying files"
 if [ "$1" == "bhac" ] ;
 then
-	cp $RAPTOR/model/bhac/* .
-
+	cp -v -n $RAPTOR/model/bhac/* .
 elif  [ "$1" == "harm3d" ] ;
 then
- cp $RAPTOR/model/harm3d/* .
+        cp -v -n $RAPTOR/model/harm3d/* .
 else
  echo "no grmhd model picked. Aborting."
  exit
 fi
 
+echo " "
 echo "Succesfully initialized."
 echo " "
 
@@ -47,11 +54,8 @@ else
     echo "Compilations succesful" 
 fi
 
-
 echo " "
 echo "Deleting setup.sh to ensure you dont overwrite model files from src"
-
-rm setup.sh
 
 echo " "
 echo "Done initialization, use make all to recompile."
