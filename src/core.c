b26fb0af67d056bcf692fdd9be038f293dd47951
jordydavelaar <jordydavelaar@gmail.com>
2022-03-16 16:18:18 -0400

    put num_freq back...

--- src/core.c
+++ src/core.c
@@ -38,7 +38,6 @@ void read_model(char *argv[]) {
     fscanf(input, "%s %s %lf", temp, temp2, &CAM_SIZE_X);
     fscanf(input, "%s %s %lf", temp, temp2, &CAM_SIZE_Y);
 
-    fscanf(input, "%s %s %d", temp, temp2, &num_frequencies);
     fscanf(input, "%s %s %d", temp, temp2, &FREQS_PER_DEC);
     fscanf(input, "%s %s %lf", temp, temp2, &FREQ_MIN);
     fscanf(input, "%s %s %lf", temp, temp2, &STEPSIZE);
