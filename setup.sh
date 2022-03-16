d7d25d2ba909f751c45241f18009377f192496d9
Jordy Davelaar <jdavelaar@pcn-10-52.cm.cluster>
2022-03-16 13:00:50 -0700

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "RAPTOR setup script"
+echo "seting up code for" $1
+
+echo " "
+
+
+if [ "$RAPTOR" == "" ] ;
+then
+	echo "Code dir is empty, set RAPTOR enviroment variable first"
+	echo "export RAPTOR=/path/to/code"
+	echo "Aborting..."
+	return 1
+else
+	echo "Code located at $RAPTOR"
+	echo " "
+fi
+
+cp $RAPTOR/run/* .
+
+if [ "$1" == "bhac" ] ;
+then
+	cp $RAPTOR/model/bhac/* .
+fi
+
+if  [ "$1" == "harm3d" ] ;
+then
+       	cp $RAPTOR/model/harm3d/*.c .
+fi
+
+echo "Succesfully initialized."
+echo " "
+
+echo "Start compilation"
+echo " " 
+make all
+
+echo " "
+echo "Deleting setup.sh to ensure you dont overwrite model files from src"
+
+rm setup.sh
+
+echo " "
+echo "Done initialization, use make all to recompile."
