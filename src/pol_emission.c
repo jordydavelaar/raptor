51e40106729cea74fe7ec49b9960c1bdd066bb65
Bram van den Berg <101322921+BramvandenBerg@users.noreply.github.com>
2022-04-06 15:38:52 +0200

    Update pol_emission.c
    Added correct thermal and kappa branches for J and 2/3 of A. ; missing.

--- src/pol_emission.c
+++ src/pol_emission.c
@@ -68,70 +68,141 @@ double rho_V(double theta_e, double n_e, double nu, double B, double theta_B, do
 
 //////////////////////////////emission and absorption J and A
 /////////factors
-J_low_factor_I=1
-J_low_factor_Q=0.5
-J_low_factor_V=(pow(0.75,2.)*pow((pow(sth,-12./25.)-1),12./25.)*pow(kappa,-66./125.)*pow(X_kappa,-7./20.)/w)
-J_high_factor_I=1
-J_high_factor_Q=(pow((4/5),2.)+kappa/50)
-J_high_factor_V=(pow((7/8),2.)*pow((pow(sth,-5./2.)-1),11./25.)*pow(kappa,-11./25.)*pow(X_kappa,-1./2.)/w)
-J_S_factor_I=1
-J_S_factor_Q=(-1)
-J_S_factor_V=(sgn(cos(theta)))
-J_x_I=3*pow(kappa,-3./2.)
-J_x_Q=(37/10)*pow(kappa,-8./5.)
-J_x_V=3*pow(kappa,-3./2.)
-A_low_factor_I=1
-A_low_factor_Q=(25/48)
+
+
+
+
+
+
+
+
+
+
 A_low_factor_V=((77/(100*w))*pow((pow(sth,-114./50.)-1),223./500.)*pow(X_kappa,-7./20.)*pow(kappa,-7./10.))
-A_high_factor_I=(pow((3/kappa),19./4.)+(3/5))
-A_high_factor_Q=(pow(21,2.)*pow(kappa,-pow(12./5.,2.))+(11/20))
+
+
 A_high_factor_V=((143/10)*pow(w,-116./125.)*pow((pow(sth,-41./20.)-1),1./2.)*(pow(13,2)*pow(kappa,-8)+(13/2500)*kappa-(263/5000)+(47/(200*kappa)))*pow(X_kappa,-1./2.))
-A_x_I=pow((-7./4. + kappa*8./5.),-43./50.)
-A_x_Q=(7./5.)*pow(kappa,-23./20.)
+
+
 A_x_V=((61./50.)*pow(kappa,-142./125.)+(7./1000.))
-///////////J_low and high and bridging function J_S
+	
+	
+	
+	
+	
+	
+	
+///////////J for thermal and kappa
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+double J_I(double theta_e, double n_e, double nu, double B, double theta_B) {
+    	
+	double J_S_I_kappa(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double J_low_factor_I=1
+		double J_high_factor_I=1
+		double J_S_factor_I=1
+		double J_x_I=3*pow(kappa,-3./2.)
+		double J_I_low_kappa= pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
+            		(pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_I
+		double J_I_high_kappa= pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
+             		(kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
+             		tgamma(kappa / 4. + 4. / 3.)*J_high_factor_I
+		
+		return pow((pow(J_I_low_kappa, -J_x_I) + pow(J_I_high_kappa, -J_x_I)), -1. / J_x_I)*J_S_factor_I
+		}
+	double j_I_thermal(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double I_I(double x) {
+    			return 2.5651 * (1 + 1.92 * pow(x, -1. / 3.) + 0.9977 * pow(x, -2. / 3.)) *
+           			exp(-1.8899 * pow(x, 1. / 3.));
+				}
+    		double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
+                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
+                      theta_e;
 
-double J_I_low(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
-            (pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_I;
+    		double x = nu / nu_c;
+
+    		return n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / 2. / sqrt(3.) /
+           		SPEED_OF_LIGHT / theta_e / theta_e * I_I(x);
+		}
+	if(DF==kappa)
+		return J_S_I_kappa(double theta_e, double n_e, double nu, double B, double theta_B)
+	if(DF==thermal)
+		return j_I_thermal(double theta_e, double n_e, double nu, double B, double theta_B)
 }
-double J_Q_low(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
-            (pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_Q;
-}
-double J_V_low(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
-            (pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_V;
+		
+double J_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
+    	
+	double J_S_Q_kappa(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double J_low_factor_Q=0.5
+		double J_high_factor_Q=(pow((4/5),2.)+kappa/50)
+		double J_S_factor_Q=(-1)
+		double J_x_Q=(37/10)*pow(kappa,-8./5.)
+		double J_Q_low_kappa= pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
+            				(pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_Q
+		double J_Q_high_kappa= pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
+             				(kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
+             				tgamma(kappa / 4. + 4. / 3.)*J_high_factor_Q
+		
+		return pow((pow(J_Q_low_kappa, -J_x_Q) + pow(J_Q_high_kappa, -J_x_Q)), -1. / J_x_Q)*J_S_factor_Q
+		}
+	double j_Q_thermal(double theta_e, double n_e, double nu, double B, double theta_B) {
+    		double I_Q(double x) {
+    			return 2.5651 *
+           			(1 + 0.93193 * pow(x, -1. / 3.) + 0.499873 * pow(x, -2. / 3.)) *
+           			exp(-1.8899 * pow(x, 1. / 3.));
+				}
+		double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
+                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
+                      theta_e;
+
+    		double x = nu / nu_c;
+
+    		return n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / 2. / sqrt(3.) /
+           		SPEED_OF_LIGHT / theta_e / theta_e * I_Q(x);
+		}
+	if(DF==kappa)
+		return J_S_Q_kappa(double theta_e, double n_e, double nu, double B, double theta_B)
+	if(DF==thermal)
+		return j_Q_thermal(double theta_e, double n_e, double nu, double B, double theta_B)
 }
 
-double J_I_high(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
-             (kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
-             tgamma(kappa / 4. + 4. / 3.)*J_high_factor_I;
-}
+double J_V(double theta_e, double n_e, double nu, double B, double theta_B) {
+    	
+	double J_S_V_kappa(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double J_low_factor_V=(pow(0.75,2.)*pow((pow(sth,-12./25.)-1),12./25.)*pow(kappa,-66./125.)*pow(X_kappa,-7./20.)/w)
+		double J_high_factor_V=(pow((7/8),2.)*pow((pow(sth,-5./2.)-1),11./25.)*pow(kappa,-11./25.)*pow(X_kappa,-1./2.)/w)
+		double J_S_factor_V=(sgn(cos(theta)))
+		double J_x_V=3*pow(kappa,-3./2.)
+		double J_V_low_kappa= pow(X_kappa,1./3.)*sth*4*M_PI* tgamma(kappa - 4. / 3.) /
+            				(pow(3, 7. / 3.) * tgamma(kappa - 2.)) * J_low_factor_V;
+		double J_V_high_kappa= pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
+             				(kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
+             				tgamma(kappa / 4. + 4. / 3.)*J_high_factor_V
+		
+		return pow((pow(J_V_low_kappa, -J_x_V) + pow(J_V_high_kappa, -J_x_V)), -1. / J_x_V)*J_S_factor_V
+		}
+	double j_V_thermal(double theta_e, double n_e, double nu, double B, double theta_B) {
+    		double I_V(double x) {
+    			return (1.81348 / x + 3.42319 * pow(x, -2. / 3.) +
+            			0.0292545 * pow(x, -0.5) + 2.03773 * pow(x, -1. / 3.)) *
+           			exp(-1.8899 * pow(x, 1. / 3.));
+				}
+		double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
+                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
+                      theta_e;
 
-double J_Q_high(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
-             (kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
-             tgamma(kappa / 4. + 4. / 3.)*J_high_factor_Q;
-}
+    		double x = nu / nu_c;
 
-double J_V_high(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow(X_kappa, -(kappa - 2) / 2.) * sth * pow(3, (kappa - 1) / 2.) *
-             (kappa - 2.) * (kappa - 1.) / 4. * tgamma(kappa / 4. - 1. / 3.) *
-             tgamma(kappa / 4. + 4. / 3.)*J_high_factor_V;
-}
+    		return 2. * n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / tan(theta_B) /
+           		3. / sqrt(3.) / SPEED_OF_LIGHT / theta_e / theta_e / theta_e *
+           		I_V(x);
+			}
 
-double J_S_I(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow((pow(J_low, -x) + pow(J_high, -x)), -1. / x)*J_S_factor_I;
+	if(DF==kappa)
+		return J_S_V_kappa(double theta_e, double n_e, double nu, double B, double theta_B)
+	if(DF==thermal)
+		return j_V_thermal(double theta_e, double n_e, double nu, double B, double theta_B)
 }
-double J_S_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow((pow(J_low, -x) + pow(J_high, -x)), -1. / x)*J_S_factor_Q;
-}
-double J_S_V(double theta_e, double n_e, double nu, double B, double theta_B) {
-    return pow((pow(J_low, -x) + pow(J_high, -x)), -1. / x)*J_S_factor_V;
-}
-///////////////////////Absorption A_low and A_high and bridging function A_S
+///////////A for thermal and kappa
 double a = kappa - 1. / 3.;
 double b = kappa + 1.;
 double c = kappa + 2. / 3.;
@@ -142,8 +213,71 @@ double hyp2F1 = pow(1. - z, -a) * tgamma(c) * tgamma(b - a) /
                     pow(1. - z, -b) * tgamma(c) * tgamma(a - b) /
                         (tgamma(a) * tgamma(c - b)) *
                         gsl_sf_hyperg_2F1(b, c - a, b - a + 1., 1. / (1. - z));
+double A_I(double theta_e, double n_e, double nu, double B, double theta_B) {
+    	
+	double A_S_I_kappa(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double A_low_factor_I=1
+		double A_high_factor_I=(pow((3/kappa),19./4.)+(3/5))
+		double A_S_factor_I=1
+		double A_x_I=pow((-7./4. + kappa*8./5.),-43./50.)
+		double A_I_low_kappa= pow(X_kappa, -2. / 3.) * pow(3, 1. / 6.) * (10. / 41.) *
+            		2*M_PI / pow(w * kappa, 10. / 3. - kappa) * (kappa - 2.) *
+            		(kappa - 1.) * kappa / (3. * kappa - 1.) * tgamma(5. / 3.) * hyp2F1*A_low_factor_I
+		double A_I_high_kappa= pow(X_kappa, -(1. + kappa) / 2.) * (pow(M_PI, 3. / 2.) / 3.) *
+             		((kappa - 2.) * (kappa - 1.) * kappa / pow(w * kappa, 3.)) *
+             		(2 * tgamma(2. + kappa / 2.) / (2. + kappa) - 1.) *A_high_Factor_I;
 
-ouble A_I_low(double theta_e, double n_e, double nu, double B, double theta_B) {
+		
+		return pow((pow(A_I_low_kappa, -A_x_I) + pow(A_I_high_kappa, -A_x_I)), -1. / A_x_I)*A_S_factor_I
+		}
+	double A_I_thermal(double theta_e, double n_e, double nu, double B, double theta_B) {
+		?
+		}
+	if(DF==kappa)
+		return A_S_I_kappa(double theta_e, double n_e, double nu, double B, double theta_B)
+	if(DF==thermal)
+		return A_I_thermal(double theta_e, double n_e, double nu, double B, double theta_B)
+}
+		
+double A_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
+    	
+	double A_S_Q_kappa(double theta_e, double n_e, double nu, double B, double theta_B) {
+		double A_low_factor_Q=(25/48)
+		double A_high_factor_Q=(pow(21,2.)*pow(kappa,-pow(12./5.,2.))+(11/20))
+		double A_S_factor_Q=(-1)
+		double A_x_Q=(7./5.)*pow(kappa,-23./20.)
+		double A_Q_low_kappa= pow(X_kappa, -2. / 3.) * pow(3, 1. / 6.) * (10. / 41.) *
+            		2*M_PI / pow(w * kappa, 10. / 3. - kappa) * (kappa - 2.) *
+            		(kappa - 1.) * kappa / (3. * kappa - 1.) * tgamma(5. / 3.) * hyp2F1*A_low_factor_Q;
+		double A_Q_high_kappa= pow(X_kappa, -(1. + kappa) / 2.) * (pow(M_PI, 3. / 2.) / 3.) *
+             		((kappa - 2.) * (kappa - 1.) * kappa / pow(w * kappa, 3.)) *
+             		(2 * tgamma(2. + kappa / 2.) / (2. + kappa) - 1.) *A_high_Factor_Q;
+		
+		return pow((pow(A_Q_low_kappa, -A_x_Q) + pow(A_Q_high_kappa, -A_x_Q)), -1. / A_x_Q)*A_S_factor_Q
+		}
+	double A_Q_thermal(double theta_e, double n_e, double nu, double B, double theta_B) {
+    		?
+		}
+	if(DF==kappa)
+		return A_S_Q_kappa(double theta_e, double n_e, double nu, double B, double theta_B)
+	if(DF==thermal)
+		return A_Q_thermal(double theta_e, double n_e, double nu, double B, double theta_B)
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+///////////////////////Absorption A_low and A_high and bridging function A_S
+
+double A_I_low(double theta_e, double n_e, double nu, double B, double theta_B) {
     return pow(X_kappa, -2. / 3.) * pow(3, 1. / 6.) * (10. / 41.) *
             2*M_PI / pow(w * kappa, 10. / 3. - kappa) * (kappa - 2.) *
             (kappa - 1.) * kappa / (3. * kappa - 1.) * tgamma(5. / 3.) * hyp2F1*A_low_factor_I;
@@ -180,10 +314,10 @@ double A_V_high(double theta_e, double n_e, double nu, double B, double theta_B)
 double A_S_I(double theta_e, double n_e, double nu, double B, double theta_B) {
     return pow((pow(A_low, -x) + pow(A_high, -x)), -1. / x)*A_S_factor_I;
 }
-double J_S_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
+double A_S_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
     return pow((pow(A_low, -x) + pow(A_high, -x)), -1. / x)*A_S_factor_Q;
 }
-double J_S_V(double theta_e, double n_e, double nu, double B, double theta_B) {
+double A_S_V(double theta_e, double n_e, double nu, double B, double theta_B) {
     return pow((pow(A_low, -x) + pow(A_high, -x)), -1. / x)*A_S_factor_V;
 }
 
@@ -201,62 +335,7 @@ double J_S_V(double theta_e, double n_e, double nu, double B, double theta_B) {
 /////////////////////////
 //kappa implementation started
 
-// Dexter (2016) A.18
-double I_I(double x) {
-    return 2.5651 * (1 + 1.92 * pow(x, -1. / 3.) + 0.9977 * pow(x, -2. / 3.)) *
-           exp(-1.8899 * pow(x, 1. / 3.));
-}
 
-// A.19
-double I_Q(double x) {
-    return 2.5651 *
-           (1 + 0.93193 * pow(x, -1. / 3.) + 0.499873 * pow(x, -2. / 3.)) *
-           exp(-1.8899 * pow(x, 1. / 3.));
-}
-
-// A.20
-double I_V(double x) {
-    return (1.81348 / x + 3.42319 * pow(x, -2. / 3.) +
-            0.0292545 * pow(x, -0.5) + 2.03773 * pow(x, -1. / 3.)) *
-           exp(-1.8899 * pow(x, 1. / 3.));
-}
-
-// A.12
-double j_I(double theta_e, double n_e, double nu, double B, double theta_B) {
-    double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
-                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
-                      theta_e;
-
-    double x = nu / nu_c;
-
-    return n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / 2. / sqrt(3.) /
-           SPEED_OF_LIGHT / theta_e / theta_e * I_I(x);
-}
-
-// A.13
-double j_Q(double theta_e, double n_e, double nu, double B, double theta_B) {
-    double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
-                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
-                      theta_e;
-
-    double x = nu / nu_c;
-
-    return n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / 2. / sqrt(3.) /
-           SPEED_OF_LIGHT / theta_e / theta_e * I_Q(x);
-}
-
-// A.14 (theta_B = pitch angle, k dot B)
-double j_V(double theta_e, double n_e, double nu, double B, double theta_B) {
-    double nu_c = 3.0 * ELECTRON_CHARGE * B * sin(theta_B) /
-                      (4.0 * M_PI * ELECTRON_MASS * SPEED_OF_LIGHT) * theta_e *
-                      theta_e;
-
-    double x = nu / nu_c;
-
-    return 2. * n_e * ELECTRON_CHARGE * ELECTRON_CHARGE * nu / tan(theta_B) /
-           3. / sqrt(3.) / SPEED_OF_LIGHT / theta_e / theta_e / theta_e *
-           I_V(x);
-}
 
 // ABSORPTION
 /////////////
