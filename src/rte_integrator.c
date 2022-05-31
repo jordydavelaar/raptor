b35f6d00bf6752607341d9f4339c0ee898e1b051
jordydavelaar <jordydavelaar@gmail.com>
2022-05-31 16:26:53 +0200

    moved unpolarized function to new folder

--- src/rte_integrator.c
+++ src/rte_integrator.c
@@ -11,21 +11,35 @@
 #include <math.h>
 #include <stdlib.h>
 
-double radiative_transfer(double *lightpath, int steps, double frequency) {
+double radiative_transfer_unpolarized(double *lightpath, int steps,
+                                      double frequency, double *IQUV) {
+
     int path_counter;
-    double I_current = 0.;
-    double j_nu = 0.;
-    double pitch_ang, nu_p, dl_current;
+    double pitch_ang, nu_p;
 
-    double X_u[4], k_u[4], k_d[4];
+    double X_u[4], k_d[4], k_u[4], dl_current;
+    double jI, jQ, jU, jV, rQ, rU, rV, aI, aQ, aU, aV;
+
     double Rg = GGRAV * MBH / SPEED_OF_LIGHT / SPEED_OF_LIGHT; // Rg in cm
+    double g_dd[4][4], g_uu[4][4];
 
-    double a_nu = 0.;
+    double Icurrent = IQUV[0];
+
     double dtau_old = 0;
 
     struct GRMHD modvar;
+    modvar.B = 0;
+    modvar.n_e = 0.;
+    modvar.theta_e = 0;
 
-    // Move backward along constructed lightpath
+    LOOP_i {
+        modvar.B_u[i] = 0;
+        modvar.U_u[i] = 0;
+        modvar.B_d[i] = 0;
+        modvar.U_d[i] = 0;
+    }
+    modvar.igrid_c = -1;
+
     for (path_counter = steps - 1; path_counter > 0; path_counter--) {
         // Current position, wave vector, and dlambda
         LOOP_i {
@@ -34,69 +48,83 @@ double radiative_transfer(double *lightpath, int steps, double frequency) {
         }
         dl_current = fabs(lightpath[(path_counter - 1) * 9 + 8]);
 
-        // Obtain the parameters n_e, THETA_e, B, and Uplasma_u at X_u
-        // get_plasma_parameters(X_u, &n_e, &THETA_e, &B, Uplasma_u);
+        metric_dd(X_u, g_dd);
+        metric_uu(X_u, g_uu);
 
-        // Check whether the ray is currently in the GRMHD simulation volume
         if (get_fluid_params(X_u, &modvar)) {
             // Obtain pitch angle: still no units (geometric)
+            lower_index(X_u, k_u, k_d);
             pitch_ang = pitch_angle(X_u, k_u, modvar.B_u, modvar.U_u);
+
+            dl_current *= (ELECTRON_MASS * SPEED_OF_LIGHT * SPEED_OF_LIGHT) /
+                          (PLANCK_CONSTANT * frequency);
 
             // CGS UNITS USED FROM HERE ON OUT
             //////////////////////////////////
+            //  if(tau[f] < log(1000.) ) {
 
             // Scale the wave vector to correct energy
             LOOP_i k_u[i] *= PLANCK_CONSTANT * frequency /
                              (ELECTRON_MASS * SPEED_OF_LIGHT * SPEED_OF_LIGHT);
-
-            // Convert distance dlambda accordingly
-            dl_current *= (ELECTRON_MASS * SPEED_OF_LIGHT * SPEED_OF_LIGHT) /
-                          (PLANCK_CONSTANT * frequency);
 
             // lower the index of the wavevector
             lower_index(X_u, k_u, k_d);
 
             // Compute the photon frequency in the plasma frame:
             nu_p = freq_in_plasma_frame(modvar.U_u, k_d);
-
             // Obtain emission coefficient in current plasma conditions
-            j_nu = emission_coeff_THSYNCHAV(modvar.B, modvar.theta_e, nu_p,
-                                            modvar.n_e);
+            evaluate_coeffs(&jI, &jQ, &jU, &jV, &rQ, &rU, &rV, &aI, &aQ, &aU,
+                            &aV, nu_p, modvar.theta_e, modvar.n_e, modvar.B,
+                            pitch_ang);
 
-            // Obtain absorption coefficient
-            if (ABSORPTION) {
-                a_nu = absorption_coeff_TH(j_nu, nu_p, modvar.theta_e);
-            }
-
-            // Constant used in integration (to produce correct units)
             double C = Rg * PLANCK_CONSTANT /
                        (ELECTRON_MASS * SPEED_OF_LIGHT * SPEED_OF_LIGHT);
 
-            double dtau = (nu_p * a_nu * dl_current * C + dtau_old);
-            double K_inv = (nu_p * a_nu);
-            double j_inv = (j_nu / (nu_p * nu_p));
+            double dtau = (aI * dl_current * C + dtau_old);
+            double K_inv = aI;
+            double j_inv = jI;
 
-            // Only add I_current if it is not NaN
-            if (j_nu == j_nu &&
-                exp(X_u[1]) <
-                    RT_OUTER_CUTOFF) { // I_current += exp(-tau) * j_nu /
-                                       // nu_p / nu_p * dl_current * C;
+            if (jI == jI) { // I_current += exp(-tau) * j_nu /
+                            // nu_p / nu_p * dl_current * C;
                 //         I_current += dI; // Old way of integrating
-                double Ii = I_current;
+                double Ii = Icurrent;
                 double S = j_inv / K_inv;
                 if (K_inv == 0)
-                    I_current = Ii;
+                    Icurrent = Ii;
                 else if (dtau < 1.e-5)
-                    I_current = Ii - (Ii - S) * (0.166666667 * dtau *
-                                                 (6. - dtau * (3. - dtau)));
+                    Icurrent = Ii - (Ii - S) * (0.166666667 * dtau *
+                                                (6. - dtau * (3. - dtau)));
                 else {
                     double efac = exp(-dtau);
-                    I_current = Ii * efac + S * (1. - efac);
+                    Icurrent = Ii * efac + S * (1. - efac);
                 }
             }
+            dtau_old = 0;
+
+            //      }
         }
+
+        /*
+           if(0 && (j_nu != j_nu || isnan(j_nu))){
+           printf("NaN emissivity! X_u[2], theta = %+.15e %+.15e\n",
+           X_u[2],M_PI*X_u[2] + 2*0.35/M_PI *
+           sin(2*M_PI*X_u[2])*atan2(2.*(log(50.)-X_u[1]),1.)); printf("NaN
+           emissivity! expX_u[1], innercutoff = %+.15e %e\n",
+           exp(X_u[1]),cutoff_inner); printf("NaN emissivity! pitch_angle =
+           %+.15e\n", pitch_ang); printf("NaN emissivity! B = %+.15e\n", B);
+           printf("NaN emissivity! THETAe = %+.15e\n", THETA_e);
+           printf("NaN emissivity! nu_plasmaframe = %+.15e\n", nu_p);
+           printf("NaN emissivity! n_e = %+.15e\n", n_e);
+           printf("NaN emissivity! j_nu = %+.15e\n", j_nu);
+           printf("NaN emissivity! tau = %+.15e\n", tau);
+           printf("NaN emissivity! U dot U = %+.15e\n\n", inner_product(X_u,
+           Uplasma_u, Uplasma_u)); printf("NaN emissivity! k dot k =
+           %+.15e\n\n", inner_product(X_u, k_u, k_u));
+           }
+         */
     }
 
-    // Store integrated intensity in the image
-    return I_current * pow(frequency, 3.);
+    IQUV[0] = Icurrent * pow(frequency, 3.);
+
+    return 1;
 }
