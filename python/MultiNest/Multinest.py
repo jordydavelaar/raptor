75f6a4bde7d5ac1d4751735da2e4b03fd1774fae
Renze Oosterhuis <roosterhuis@coma.science.ru.nl>
2022-06-28 16:11:45 +0200

    MultiNest dev

--- python/MultiNest/Multinest.py
+++ python/MultiNest/Multinest.py
@@ -1,15 +1,20 @@
 '''
 Set parameters in 'MultiNestRAPTORinput.txt'
-Run this file with command: python Multinest.py Run-name
+Check if there are enough lines for the frequencies in file 'img_renderer.c'
+Run this file with command: python MultiNestRAPTOR_SgrA.py Run-name
 
-To run this code, one has to have downloaded PyMultiNest
+To run this code, one has to have downloaded PyMultiNest, ehtim
 
 Needed files:
-        Multinest.py
+	Multinest.py
 	MultiNestRAPTORinput.txt
 	Convergenceplotter.py
-	
-	spectrum.txt
+
+Maybe needed files:
+	frequencies.txt
+	Observational_Spectrum.txt
+	Observational_Coreshift.txt
+        Observational_Image.txt
 
 Not used in this code, but needed to make plots of the best fits afterwards
 	plotterSgrAalles.py
@@ -22,6 +27,11 @@ Output consists of:
 	Run-name_Convergence.png
 	Run-name_livepoints.txt
 	Run-name_modes.txt
+
+Developped by:
+Joost de Kleuver, Renze Oosterhuis
+
+De oplossing van de formule? Moleculen zonder scrupules
 '''
 #====================================================================================================
 # Import packages
@@ -35,24 +45,25 @@ import sys
 import pymultinest
 from pymultinest.solve import solve
 from pymultinest.analyse import Analyzer
+import ehtim as eh
 
 # Define global variables
-MBH_var, M_UNIT_var, Rhigh_var, i_var = [0,0,0,0]
-num_Radio, num_indices = [0,0]
+MBH_var, M_UNIT_var, Rhigh_var, i_var, Radio, Coreshift, Image = [0,0,0,0,0,0,0]
+num_Radio, num_IR, num_Coreshift, num_indices = [0,0,0,0]
 MBH, M_UNIT, Rhigh, i, data_number = [0,0,0,0,0]
 loglike_evaluation = 0
 
 # Define functions
 def Initialize_modelin(params_modelin):
 	# Set parameters in 'model.in'
-	MBH, M_UNIT, Rlow, Rhigh, i, IMG_WIDTH, IMG_HEIGHT, CAM_SIZE_X, CAM_SIZE_Y, FREQS_PER_DEC, FREQ_MIN, STEPSIZE, MAX_LEVEL = params_modelin
+	MBH, M_UNIT, Rhigh, Rlow, i, IMG_WIDTH, IMG_HEIGHT, CAM_SIZE_X, CAM_SIZE_Y, FREQS_PER_DEC, FREQ_MIN, STEPSIZE, MAX_LEVEL = params_modelin
 	f = open('model.in','r')
 	text = f.readlines()
 	f.close()
 	text[0] = 'MBH\t\t(g)\t\t%.15e\n'%(MBH)
 	text[1] = 'M_UNIT\t\t(g)\t\t%.15e\n'%(M_UNIT)
-	text[2] = 'Rlow\t\t(-)\t\t%d\n'%(Rlow)
-	text[3] = 'Rhigh\t\t(-)\t\t%d\n'%(Rhigh)
+	text[2] = 'Rhigh\t\t(-)\t\t%d\n'%(Rhigh)
+	text[3] = 'Rlow\t\t(-)\t\t%d\n'%(Rlow)
 	text[5] = 'INCLINATION\t(deg)\t%d\n'%(i)
 	text[6] = 'IMG_WIDTH\t(pixels)\t%d\n'%(IMG_WIDTH)
 	text[7] = 'IMG_HEIGHT\t(pixels)\t%d\n'%(IMG_HEIGHT)
@@ -61,7 +72,7 @@ def Initialize_modelin(params_modelin):
 	text[10] = 'FREQS_PER_DEC\t(-)\t\t%d\n'%(FREQS_PER_DEC)
 	text[11] = 'FREQ_MIN\t(Hz)\t\t%.15e\n'%(FREQ_MIN)
 	text[12] = 'STEPSIZE\t(-)\t\t%.15e\n'%(STEPSIZE)
-	text[13] = 'MAX_LEVEL\t(-)\t\t%d\n'%(MAX_LEVEL)
+	text[10] = 'MAX_LEVEL\t(-)\t\t%d\n'%(MAX_LEVEL)
 	f = open('model.in','w')
 	f.writelines(text)
 	f.close()
@@ -69,23 +80,22 @@ def Initialize_modelin(params_modelin):
 def Set_parameters():
     	# Load parameters from 'MultiNestRAPTORinput.txt'
 	_, params = np.loadtxt('MultiNestRAPTORinput.txt', dtype=str, delimiter='=', unpack=True)
-	params = params.astype(float)
+	params = params[0:27].astype(float)
        	# Set global variables
-	global MBH_var, M_UNIT_var, Rhigh_var, i_var
-	MBH_var, M_UNIT_var, Rhigh_var, i_var = params[0:4].astype(int)
+	global MBH_var, M_UNIT_var, Rhigh_var, i_var, Radio, IR, Coreshift
+	MBH_var, M_UNIT_var, Rhigh_var, i_var, Radio, IR, Coreshift = params[0:7].astype(int)
 	global MBH, M_UNIT, Rhigh, i, data_number
 	if not MBH_var:
-		MBH = params[4]
+		MBH = params[7]
 	if not M_UNIT_var:
-		M_UNIT = params[5]
+		M_UNIT = params[8]
 	if not Rhigh_var:
-		Rhigh = params[6]
+		Rhigh = params[9]
 	if not i_var:
-		i = params[8]
-	#data_number = params[26]
-	data_number = 0
+		i = params[11]
+	data_number = params[26]
     	# Initialize input files RAPTOR
-	Initialize_modelin(params[4:17])
+	Initialize_modelin(params[7:20])
 
 def Set_modelin(MBH, M_UNIT,Rhigh,i):
         # Set MBH, M_UNIT, Rhigh and i in 'model.in'
@@ -94,7 +104,7 @@ def Set_modelin(MBH, M_UNIT,Rhigh,i):
         f.close()
         text[0] = 'MBH\t\t(g)\t\t%.15e\n'%(MBH)
         text[1] = 'M_UNIT\t\t(g)\t\t%.15e\n'%(M_UNIT)
-        text[3] = 'Rhigh\t\t(-)\t\t%d\n'%(Rhigh)
+        text[2] = 'Rhigh\t\t(-)\t\t%d\n'%(Rhigh)
         text[5] = 'INCLINATION\t(deg)\t%d\n'%(i)
         f = open('model.in','w')
         f.writelines(text)
@@ -102,9 +112,10 @@ def Set_modelin(MBH, M_UNIT,Rhigh,i):
 
 def RAPTOR(MBH, M_UNIT, Rhigh, i, data_number):
         # Set parameter values for RAPTOR:
+        print("running raptor")
         Set_modelin(MBH, M_UNIT, Rhigh,i)
     	# Run RAPTOR
-        os.system('./RAPTOR model.in ../data1000.dat %d'%(data_number))
+        os.system('./RAPTOR model.in ./harm3d.txt %d'%(data_number))
     	# Load flux values
         Freq, Flux = np.loadtxt('output/spectrum_%d_%d.00.dat'%(data_number,np.floor(i)), dtype=str, unpack=True)
         Freq = Freq.astype(float)
@@ -112,26 +123,26 @@ def RAPTOR(MBH, M_UNIT, Rhigh, i, data_number):
     	# Write output to 'Run-name_flux.txt'
         f = open("%s_Flux.txt"%(sys.argv[1]),'a+')
         f.write(str(Freq)+' '+str(Flux)+'\n')
+        # f.write(' '.join(map(str,Flux))+'\n')
         f.close()
     	# Return flux values
+        # 
         return Flux
 
 def myprior(cube, ndim=MBH_var+M_UNIT_var+Rhigh_var+i_var, nparams=MBH_var+M_UNIT_var+Rhigh_var+i_var):
     	if MBH_var: # MBH has a log uniform distribution between 1.0e40 and 1.0e42 g
-        	cube[MBH_var-1] = 10**(2.0*cube[MBH_var-1] + 40)
-    	if M_UNIT_var: # M_unit has a log uniform distribution between 1e19 and 1e25 g
-        	cube[MBH_var+M_UNIT_var-1] = 10**(6*cube[MBH_var+M_UNIT_var-1] + 19)
-    	if Rhigh_var: # Rhigh has a log uniform distribution between 10 and 100
-        	cube[MBH_var+M_UNIT_var+Rhigh_var-1] = 10**(cube[MBH_var+M_UNIT_var+Rhigh_var-1] + 1)
+        	cube[MBH_var-1] = 10**(2.0*cube[MBH_var-1] + 39)
+    	if M_UNIT_var: # M_unit has a log uniform distribution between 1e22 and 1e24 g
+        	cube[MBH_var+M_UNIT_var-1] = 10**(2*cube[MBH_var+M_UNIT_var-1] + 22)
+    	if Rhigh_var: # Rhigh has a uniform distribution between 1 and 100
+        	cube[MBH_var+M_UNIT_var+Rhigh_var-1] = 99*cube[MBH_var+M_UNIT_var+Rhigh_var-1] + 1
     	if i_var: # i has a uniform distribution between 12 and 45
         	cube[MBH_var+M_UNIT_var+Rhigh_var+i_var-1] = 33*cube[MBH_var+M_UNIT_var+Rhigh_var+i_var-1] + 12
     	return cube
 
 def myloglike(cube, ndim=MBH_var+M_UNIT_var+Rhigh_var+i_var, nparams=MBH_var+M_UNIT_var+Rhigh_var+i_var):
         global loglike_evaluation
-        global data_number
         loglike_evaluation += 1
-        data_number += 1
 	# Get flux values
         global MBH, M_UNIT, Rhigh, i
         if MBH_var:
@@ -142,19 +153,21 @@ def myloglike(cube, ndim=MBH_var+M_UNIT_var+Rhigh_var+i_var, nparams=MBH_var+M_U
                 Rhigh = cube[MBH_var+M_UNIT_var+Rhigh_var-1]
         if i_var:
                 i = cube[MBH_var+M_UNIT_var+Rhigh_var+i_var-1]
-        Flux = RAPTOR(MBH, M_UNIT, Rhigh, i, data_number)
-    	# Calculate chisquared values for radio, IR and Coreshift
-        _, Measurements_Radio = np.loadtxt('spectrum.txt').transpose()
-        global num_Radio
-        num_Radio = 5
-        Chisquare_Radio = 0
-        for freqnum in range(num_Radio):
-#            	Chisquare_Radio += (Measurements_Radio[freqnum] - np.log10(Flux[freqnum]))**2  /(2*Sigma_Radio[freqnum]**2)
-                Chisquare_Radio += (Measurements_Radio[freqnum] - Flux[freqnum])**2
-#		Chisquare_Radio += (np.log10(Measurements_Radio[freqnum]) - np.log10(Flux[freqnum]))**2 /(2*(np.log10(Measurements_Radio[freqnum])-np.log10(Measurements_Radio[freqnum] - Sigma_Radio[freqnum]))**2)
+        #Calculate Chi^2
+        if Spectrum:
+                Flux = RAPTOR(MBH, M_UNIT, Rhigh, i, data_number)
+                _, Measurements_Radio, Sigma_Radio = np.loadtxt('spectrum.txt', delimiter = " ", unpack = True)
+        	global num_Radio
+        	for freqnum in range(num_Radio):
+#            		Chisquare_Radio += (Measurements_Radio[freqnum] - np.log10(Flux[freqnum]))**2  /(2*Sigma_Radio[freqnum]**2)
+        		Chisquare_Radio += (Measurements_Radio[freqnum] - Flux[freqnum])**2  /(num_Radio*2*Sigma_Radio[freqnum]**2)
+#			Chisquare_Radio += (np.log10(Measurements_Radio[freqnum]) - np.log10(Flux[freqnum]))**2 /(2*(np.log10(Measurements_Radio[freqnum])-np.log10(Measurements_Radio[freqnum] - Sigma_Radio[freqnum]))**2) #       	
+#			Chisquare_Radio +=(Measurements_Radio[freqnum] - Flux[freqnum])**2
+        # Calculate total chisquared value
+    	Chisquare_Total = Chisquare_Radio + Chisquare_Image + Chisquare_Coreshift
     	# Write Chisquared values to 'Run-name_Chisquared.txt'
         f = open("%s_Chisquared.txt"%(sys.argv[1]),'a+')
-        f.write("%.15e\t%.15e\t%.15e\t%.15e\t%.15e\n"%(MBH, M_UNIT, Rhigh, i, Chisquare_Radio))
+        f.write("%.15e\t%.15e\t%.15e\t%.15e\t%.15e\n"%(MBH, M_UNIT, Rhigh, i, Chisquare_Total))
         f.close()
 	# Write all live point info to 'Run-name_livepoints.txt'
         livepoints = np.loadtxt('%sphys_live.points'%(sys.argv[1]), dtype=str)
@@ -167,7 +180,7 @@ def myloglike(cube, ndim=MBH_var+M_UNIT_var+Rhigh_var+i_var, nparams=MBH_var+M_U
 	# Update Convergence plot
         os.system('python3 Convergenceplotter.py %s'%(sys.argv[1]))
     	# Return loglikelihood
-        return - Chisquare_Radio
+        return - Chisquare_Total
 
 ## Main code
 Set_parameters()
@@ -177,8 +190,7 @@ print(parameters)
 n_params = len(parameters)
 prefix = sys.argv[1]
 
-result = pymultinest.run(LogLikelihood=myloglike, Prior=myprior,n_dims=n_params, importance_nested_sampling=False, multimodal=True, evidence_tolerance=0.5, sampling_efficiency=0.8, null_log_evidence=-1e90,outputfiles_basename = prefix, max_iter = 0,  max_modes=5, mode_tolerance=-1e90, resume=False, verbose=True, write_output=True, n_live_points=100)
-print("gelukt")
+result = pymultinest.run(LogLikelihood=myloglike, Prior=myprior, max_iter = 0,n_dims=n_params, importance_nested_sampling=False, multimodal=True, evidence_tolerance=0.5, sampling_efficiency=0.8, null_log_evidence=-1e90,outputfiles_basename = prefix, max_modes=5, mode_tolerance=-1e90, resume=False, verbose=True, write_output=True, n_live_points=400)
 
 # Write for each mode the mean values of the parameters with the standard deviation and the log_evidence with its error to prefix_modes.txt
 a = Analyzer(n_params, outputfiles_basename="%s"%(prefix))
