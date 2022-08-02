bb942de170f996ba1de1da14a9de71fc0d7efddc
jordydavelaar <jordydavelaar@gmail.com>
2022-08-02 17:20:47 -0400

    changed SMR radii

--- src/camera.c
+++ src/camera.c
@@ -147,9 +147,9 @@ int refine_block(struct Camera intensity) {
 // begins
 int refine_init_block(struct Camera intensity) {
 
-    double radius_4 = 25;
-    double radius_3 = 50;
-    double radius_2 = 100;
+    double radius_4 = 5;
+    double radius_3 = 10;
+    double radius_2 = 15;
 
     BLOCK_SIZE_X =
         CAM_SIZE_X / (pow(2, intensity.level - 1) * (double)(num_blocks));
@@ -167,11 +167,9 @@ int refine_init_block(struct Camera intensity) {
     double rmax = fmax(rl, ru);
     double rmin = fmin(rl, ru);
 
-
     bool bool_4 = radius_4 > rmax;
     bool bool_3 = radius_3 > rmax && radius_4 < rmax;
     bool bool_2 = radius_2 > rmax && radius_3 < rmax;
-
 
     if (bool_4 && intensity.level < 4 && max_level > 3)
         return 1;
