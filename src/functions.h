f229affeb4b1d446edc28222cef461ed8584de0e
jordydavelaar <jordydavelaar@gmail.com>
2022-10-31 11:22:53 -0400

    adding tau diagnostics

--- src/functions.h
+++ src/functions.h
@@ -105,11 +105,13 @@ void integrate_geodesic(double alpha, double beta, double *lightpath,
 
 void radiative_transfer_polarized(double *lightpath, int steps,
                                   double frequency, double *f_x, double *f_y,
-                                  double *p, int PRINT_POLAR, double *IQUV);
+                                  double *p, int PRINT_POLAR, double *IQUV,
+                                  double tau, double tauF);
 
 double radiative_transfer_unpolarized(double *lightpath, int steps,
                                       double *frequency,
-                                      double IQUV[num_frequencies][4]);
+                                      double IQUV[num_frequencies][4],
+                                      double tau);
 // METRIC.C
 ///////////
 
