4dc8532b6f32b65bd607b689693880c8cbd1b1ae
jordydavelaar <jordydavelaar@gmail.com>
2022-11-15 15:19:41 -0500

    bug in plotting script

--- model/bhac/definitions.h
+++ model/bhac/definitions.h
@@ -59,6 +59,8 @@ typedef struct Camera {
     double IQUV[tot_pixels][num_frequencies][4]; // intensity
     double tau[tot_pixels][num_frequencies];     // intensity
     double tauF[tot_pixels][num_frequencies];    // intensity
+    double pdf[tot_pixels][num_frequencies];    // intensity
+    double avg[tot_pixels][num_frequencies];    // intensity
     double alpha[tot_pixels];                    // impact parameter
     double beta[tot_pixels];                     // impact parameter
     double lcorner[2];                           // lower left corner of a block
